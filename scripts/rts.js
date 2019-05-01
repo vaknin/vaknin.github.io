076700166bfbf942a421d991efa3cf386b80d1e1
Kivan <vaknins33@gmail.com>
2019-05-01 20:33:33 +0300

    rts initial commit

--- scripts/rts.js
+++ scripts/rts.js
@@ -1,16 +1,96 @@
 //#region Globals
 
+let objects = [];
+
+
+//#endregion
+
 //#region Canvas
 
+//Game aspect ratio
+const aspectRatio = 16 / 9;
+
 let canvas = document.getElementById('canvas');
-let canvasW = window.innerWidth * 0.85; 
-let canvasH = window.innerHeight * 0.85;
-canvas.width = canvasW;
-canvas.height = canvasH;
+initializeCanvas();
 let ctx = canvas.getContext('2d');
-let cx = canvas.getBoundingClientRect().x;
-let cy = canvas.getBoundingClientRect().y;
+
+//Set the canvas height accoarding to the desired aspect ratio
+function initializeCanvas(){
+
+    canvas.width = window.innerWidth;
+    let height = window.innerHeight;
+    let currentAspectRatio = window.innerWidth / window.innerHeight;
+    let diff = aspectRatio > currentAspectRatio ? -1 : 1;
+
+    //Get canvas height
+    while (true){
+
+        //Once reached the desire aspect ratio, set the canvas to that size and break
+        if ((canvas.width / height).toFixed(2) == aspectRatio.toFixed(2)){
+            canvas.height = height;
+            break;
+        }
+        
+        //Keep incrementing/decrementing the height until it reaches the desire ratio
+        else{
+            height += diff;
+        }
+    }
+}
 
 //#endregion
 
+//#region Grid
+
+let grid = [];
+
 //#endregion
+
+//#region Functions
+
+//Draw on the canvas every frame
+function draw(){
+
+    //Execute every frame
+    requestAnimationFrame(draw);
+
+    //Erase last frame
+    ctx.fillStyle = 'white';
+    ctx.fillRect(0,0, canvas.width, canvas.height);
+
+    //Draw the frame
+    objects.forEach(object => {
+        object.draw();
+    });
+
+    //Debug bg
+    ctx.fillStyle = 'teal';
+    ctx.fillRect(0,0, canvas.width, canvas.height);
+}
+
+//Main function
+function main(){
+    draw();
+}
+
+//#endregion
+
+//#region Events
+
+//Keydown listener
+document.addEventListener('keydown', e => {
+
+//    console.log(e.which);
+
+    //Variables
+    let D = e.which == 68;
+
+    //D key
+    if (D){
+    }
+    
+});
+
+//#endregion
+
+main();
